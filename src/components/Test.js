57905cca6cdada668ec49f61f8b52017050372b7
Andrew Davisson <akdavisson4@gmail.com>
2020-03-27 12:57:25 -0600

    stubbed test component

---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// Use this component to test out getting companies without authentication
+
+const Test = () => {
+  return (
+    <div className="test">
+      <h1>Testing Page</h1>
+    </div>
+  );
+}
+ 
+export default Test;
