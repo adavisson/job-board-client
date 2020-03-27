742e6a2c15811f92d03c4123bf0eb91674cec63c
Andrew Davisson <akdavisson4@gmail.com>
2020-03-27 12:54:18 -0600

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import {
   Home,
 } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
