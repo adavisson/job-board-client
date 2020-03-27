6f06fee15bcb3776a7a4c45ea531311469747b29
Andrew Davisson <akdavisson4@gmail.com>
2020-03-27 15:35:37 -0600

    stubbed Job Postings

---
+++ src/components/JobPostings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobPostings = () => {
+  return (
+    <div className="job-postings">
+      <h1>Job Postings</h1>
+    </div>
+  );
+}
+ 
+export default JobPostings;
