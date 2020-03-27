f81ce47243f56737febb5de983119a7ca1405a46
Andrew Davisson <akdavisson4@gmail.com>
2020-03-27 12:58:44 -0600

    added test component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import {
   Home,
+  Test,
 } from './components';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Home} />
+        <Route path="/test" component={Test} />
       </div>
     </Router>
   );
