35d4695347e202791dbe5b0039a7bd923e6f1fda
Andrew Davisson <akdavisson4@gmail.com>
2020-03-27 12:48:35 -0600

    stubbed home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Your Job Board</h1>
+      <p>Your futures starts here.</p>
+    </div>
+  );
+}
+ 
+export default Home;
