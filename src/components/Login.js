c01a8636629914ed2db5a1c65b0268a1e791e019
Andrew Davisson <akdavisson4@gmail.com>
2020-03-28 08:13:03 -0600

    created login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Form, Card, Col, Row, Button} from 'react-bootstrap';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`email: ${email}`);
+    console.log(`password: ${password}`)
+  }
+
   return (
-    <div className="login-form">
-      <h1>Login</h1>
-    </div>
+    <Card className="text-center">
+      <Card.Header>Login</Card.Header>
+      <Card.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group as={Row} controlId="emailInput">
+            <Form.Label column sm="2">Email: </Form.Label>
+            <Col sm="10">
+              <Form.Control type="input" value={email} onChange={e => setEmail(e.target.value)} />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="passwordInput">
+            <Form.Label column sm="2">Password: </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Col>
+          </Form.Group>
+          <Button variant="dark" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 }
  
