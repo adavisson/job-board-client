39878f56c8ba80bc75d91947f7d594244bf171a7
Andrew Davisson <akdavisson4@gmail.com>
2020-03-28 10:52:04 -0600

    work on login funtionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,33 @@
 import React, {useState} from 'react';
 import {Form, Card, Col, Row, Button} from 'react-bootstrap';
+import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
+
+const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        id
+        name
+      }
+    }
+  }
+`
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [login, { data }] = useMutation(LOGIN);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(`email: ${email}`);
     console.log(`password: ${password}`)
+
+    const result = login({ variables: {email: email, password: password}});
+    console.log(result);
   }
 
   return (
