77733642e1d269c4a8a9229d6d87c69dbe925be7
Andrew Davisson <akdavisson4@gmail.com>
2020-03-30 11:15:53 -0600

    implemented redirect if not logged in

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Heading from './Heading';
+import { AUTH_TOKEN } from '../constants';
+import { Redirect } from 'react-router';
 
 const Contacts = () => {
   const title = "Contacts"
-  
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  if (!authToken) {
+    alert('Please login to view your contacts.')
+    return <Redirect to="/login" />
+  }
+
   return (
     <div className="contacts">
       <Heading title={title} />
