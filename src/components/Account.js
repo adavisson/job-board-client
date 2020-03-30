46095de2ba0ab9c14283307f1885f8718e5cdfd5
Andrew Davisson <akdavisson4@gmail.com>
2020-03-30 15:02:33 -0600

    Queries properly

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Redirect } from 'react-router'
 import Heading from './Heading';
+import { AUTH_TOKEN } from '../constants';
+import { GET_CURRENT_USER } from '../graphql/queries';
+import { Query } from 'react-apollo';
 
 const Account = () => {
   const title="Account Page"
@@ -13,6 +17,24 @@ const Account = () => {
   return (
     <div className="account">
       <Heading title={title} />
+      <Query query={GET_CURRENT_USER}>
+        {({ loading, error, data}) => {
+          if(loading) return <p>Loading...</p>
+          if(error) return <p>Error :(</p>
+          
+          const user = data.currentUser
+
+          return (
+            <div>
+              <h3>{user.name}</h3>
+              <p>Email: {user.email}</p>
+              {user.age && <p>Age: {user.age}</p>}
+              {user.bio && <p>About: {user.bio}</p>}
+              {user.gender && <p>Gender: {user.gender}</p>}
+            </div>
+          )
+        }}
+      </Query>
     </div>
   );
 }
