117bd761a70c9448e1832bc8f56e0d4891646385
Andrew Davisson <akdavisson4@gmail.com>
2020-03-30 10:36:32 -0600

    added companies query to component

---
+++ src/components/Companies.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_COMPANIES } from '../graphql/queries';
+import Heading from './Heading';
+
+const Companies = () => {
+  const title = "Companies"
+
+  const { loading, error, data } = useQuery(GET_COMPANIES);
+
+  if (loading) return (
+    <>
+      <Heading title={title} />
+      <p>Loading...</p>
+    </>
+  )
+  if (error) return <p>Error :(</p>
+  return (
+    <div className="companies">
+      <Heading title={title} />
+      {data.companies.map(({name, address, phoneNumber, webSite}) => (
+          <>
+            <h5>{name}</h5>
+            <p>Address: {address}</p>
+            <p>Phone: {phoneNumber}</p>
+            <p>WebSite: <a href={link}>{webSite}</a></p>
+            <br/>
+          </>
+        )
+      )}
+    </div>
+  );
+}
+ 
+export default Companies;
