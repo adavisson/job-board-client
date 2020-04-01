1b492ee3c08da667ad9c8368d6f27557750138b6
Andrew Davisson <akdavisson4@gmail.com>
2020-04-01 11:06:56 -0600

    refactored to use Query component

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import { useQuery } from '@apollo/react-hooks';
+import { Query } from 'react-apollo';
 import { GET_COMPANIES } from '../graphql/queries';
 import { AUTH_TOKEN } from '../constants';
 import Heading from './Heading';
@@ -9,32 +9,36 @@ const Companies = () => {
   const title = "Companies"
   const authToken = localStorage.getItem(AUTH_TOKEN);
 
-  const { loading, error, data } = useQuery(GET_COMPANIES);
-
-  if (loading) return (
-    <>
-      <Heading title={title} />
-      <p>Loading...</p>
-    </>
-  )
-  if (error) return <p>Error :(</p>
   return (
     <div className="companies">
       <Heading title={title} />
       {authToken && (<Button variant="dark" href="/new-company">Add Company</Button>)}
-      <div className="card-container">
-        {data.companies.map(({name, address, phoneNumber, website}) => (
-            <Card className="company-card">
-              <Card.Header>{name}</Card.Header>
-              <Card.Body>
-                <Card.Text>Address: {address}</Card.Text>
-                <Card.Text>Phone: {phoneNumber}</Card.Text>
-                <Card.Text>Website: <a href={website}>{website}</a></Card.Text>
-              </Card.Body>
-            </Card>
+      <Query query={GET_COMPANIES}>
+        {({ loading, error, data }) => {
+          if (loading) return (
+            <>
+              <Heading title={title} />
+              <p>Loading...</p>
+            </>
           )
-        )}
-      </div>
+          if (error) return <p>Error :(</p>
+          return (
+            <div className="card-container">
+              {data.companies.map(({name, address, phoneNumber, website}) => (
+                  <Card className="company-card">
+                    <Card.Header>{name}</Card.Header>
+                    <Card.Body>
+                      <Card.Text>Address: {address}</Card.Text>
+                      <Card.Text>Phone: {phoneNumber}</Card.Text>
+                      <Card.Text>Website: <a href={website}>{website}</a></Card.Text>
+                    </Card.Body>
+                  </Card>
+                )
+              )}
+            </div>
+          )
+        }}
+      </Query>
     </div>
   );
 }
