3545eef1f58bde20b63a8f7c7c68d1434b170f1b
Andrew Davisson <akdavisson4@gmail.com>
2020-04-01 11:28:43 -0600

    only authenticated users can view page

--- src/components/NewContact.js
+++ src/components/NewContact.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import { AUTH_TOKEN } from '../constants';
 
 const NewContact = () => {
+  const title = "Add Contact"
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
+  if (!authToken) {
+    alert('Please login to add a contact')
+    return <Redirect to="/login" />
+  }
+
   return (  
     <h1>New Contact</h1>
   );
