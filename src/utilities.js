d41c35b0db12b8085da9a65add347f33c1d5f360
Andrew Davisson <akdavisson4@gmail.com>
2020-04-06 08:21:16 -0600

    Did not function properly

--- src/utilities.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Redirect } from 'react-router';
-
-export const loggedIn = (authToken, response) => {
-  if (!authToken) {
-    alert(response)
-    return <Redirect to="/login"/>
-  }
-}
