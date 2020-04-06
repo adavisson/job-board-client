3681337ac99a878c0b361c93feb965d3b9e10ed0
Andrew Davisson <akdavisson4@gmail.com>
2020-04-06 08:11:29 -0600

    added redirect if not authed

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Heading from './Heading';
+import { AUTH_TOKEN } from '../constants';
+import { Redirect } from 'react-router';
 
 const Notes = () => {
   const title = "Notes"
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
+  if (!authToken){
+    alert('Please login to view your notes.')
+    return <Redirect to="/login" />
+  }
+
   return (
     <Heading title={title} /> 
   );
