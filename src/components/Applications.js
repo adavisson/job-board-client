acf8f4f0b38cc6f8ebc9decc0cab9684c9ab9a48
Andrew Davisson <akdavisson4@gmail.com>
2020-04-06 09:58:54 -0600

    only authed users can view

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Heading from './Heading';
+import { Redirect } from 'react-router';
+import { AUTH_TOKEN } from '../constants';
 
 const Applications = () => {
   const title = "Applications"
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
+  if (!authToken) {
+    alert('Please login to see your applications.')
+    return <Redirect to="/login" />
+  }
+
   return (  
     <Heading title={title} />
   );
