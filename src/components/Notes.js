1357f3e75fdc5e8d1509b97b79fdaf0202729574
Andrew Davisson <akdavisson4@gmail.com>
2020-04-06 09:27:46 -0600

    new note button

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,7 @@ import { AUTH_TOKEN } from '../constants';
 import { Redirect } from 'react-router';
 import { Query } from 'react-apollo';
 import { GET_NOTES } from '../graphql/queries';
-import { Table } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 
 const Notes = () => {
   const title = "Notes"
@@ -16,48 +16,50 @@ const Notes = () => {
   }
 
   return (
-    <div className="container">
+    <div className="notes">
       <Heading title={title} />
+      <Button variant="dark">New Note</Button>
+      <div className="container">
+        <Table responsive striped bordered size="sm">
+          <thead>
+            <tr>
+              <th>Date: (last updated)</th>
+              <th>Text</th>
+              <th>Application</th>
+              <th>Company</th>
+              <th>Contact</th>
+            </tr>
+          </thead>
+          <tbody>
+            <Query query={GET_NOTES}>
+              {({loading, error, data}) => {
+                if (loading) return (
+                  <p>Loading...</p>
+                )
+                if (error) return <p>Error :(</p>
 
-      <Table responsive striped bordered size="sm">
-        <thead>
-          <tr>
-            <th>Date: (last updated)</th>
-            <th>Text</th>
-            <th>Application</th>
-            <th>Company</th>
-            <th>Contact</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Query query={GET_NOTES}>
-            {({loading, error, data}) => {
-              if (loading) return (
-                <p>Loading...</p>
-              )
-              if (error) return <p>Error :(</p>
+                data.notes.sort((a,b) => (a.updatedAt > b.updatedAt ? 1 : -1))
 
-              data.notes.sort((a,b) => (a.updatedAt > b.updatedAt ? 1 : -1))
-
-              return (
-                <>
-                  {data.notes.map(({updatedAt, body, application, company, contact}) => {
-                    return (
-                      <tr>
-                        <td>{updatedAt}</td>
-                        <td>{`${body.substring(0, 30)}...`}</td>
-                        <td>{application && (`${application.jobPosting.title} at ${application.jobPosting.company.name}`)}</td>
-                        <td>{company && company.name}</td>
-                        <td>{contact && contact.name}</td>
-                      </tr>
-                    )
-                  })}
-                </>
-              )
-            }}
-          </Query>
-        </tbody>
-      </Table>
+                return (
+                  <>
+                    {data.notes.map(({updatedAt, body, application, company, contact}) => {
+                      return (
+                        <tr>
+                          <td>{updatedAt}</td>
+                          <td>{`${body.substring(0, 30)}...`}</td>
+                          <td>{application && (`${application.jobPosting.title} at ${application.jobPosting.company.name}`)}</td>
+                          <td>{company && company.name}</td>
+                          <td>{contact && contact.name}</td>
+                        </tr>
+                      )
+                    })}
+                  </>
+                )
+              }}
+            </Query>
+          </tbody>
+        </Table>
+      </div>
     </div>
   )
 }
