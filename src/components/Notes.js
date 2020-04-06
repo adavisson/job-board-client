b1ff7eb2d29927e91af21ee1506546401a52ee46
Andrew Davisson <akdavisson4@gmail.com>
2020-04-06 09:16:40 -0600

    put notes into table

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Heading from './Heading';
 import { AUTH_TOKEN } from '../constants';
 import { Redirect } from 'react-router';
+import { Query } from 'react-apollo';
+import { GET_NOTES } from '../graphql/queries';
+import { Table } from 'react-bootstrap';
 
 const Notes = () => {
   const title = "Notes"
@@ -13,8 +16,50 @@ const Notes = () => {
   }
 
   return (
-    <Heading title={title} /> 
-  );
+    <div className="table-container">
+      <Heading title={title} />
+
+      <Table responsive striped bordered size="sm">
+        <thead>
+          <tr>
+            <th>Date: (last updated)</th>
+            <th>Text</th>
+            <th>Application</th>
+            <th>Company</th>
+            <th>Contact</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Query query={GET_NOTES}>
+            {({loading, error, data}) => {
+              if (loading) return (
+                <p>Loading...</p>
+              )
+              if (error) return <p>Error :(</p>
+
+              data.notes.sort((a,b) => (a.updatedAt > b.updatedAt ? 1 : -1))
+
+              return (
+                <>
+                  {data.notes.map(({updatedAt, body, application, company, contact}) => {
+                    return (
+                      <tr>
+                        <td>{updatedAt}</td>
+                        <td>{body}</td>
+                        <td>{application && (`${application.jobPosting.title} at ${application.jobPosting.company.name}`)}</td>
+                        <td>{company && company.name}</td>
+                        <td>{contact && contact.name}</td>
+                      </tr>
+                    )
+                  })}
+                </>
+              )
+            }}
+          </Query>
+        </tbody>
+      </Table>
+    </div>
+  )
 }
  
 export default Notes;
