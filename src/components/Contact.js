ae41c32cfeaf953a40a9ab7269d2ff5398c9fa05
Andrew Davisson <akdavisson4@gmail.com>
2020-04-07 12:52:05 -0600

    only authed users can view

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import Heading from './Heading'
+import Heading from './Heading';
+import { Redirect } from 'react-router';
+import { AUTH_TOKEN } from '../constants';
 
 const Contact = () => {
   const name = "Contact"
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
+  if (!authToken) {
+    alert('Please login to view contact.')
+    return <Redirect to="/login" />
+  }
 
   return (
     <Heading title={name} />
