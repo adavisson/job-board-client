3516126691f121bcfc19be85484ded92e064751c
Andrew Davisson <akdavisson4@gmail.com>
2020-04-07 12:49:00 -0600

    removed useState

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Heading from './Heading';
 import { Query } from 'react-apollo';
 import { Redirect } from 'react-router';
