1348dd9473da4da1a720e02a703ec632988215e6
Andrew Davisson <akdavisson4@gmail.com>
2020-04-08 12:33:29 -0600

    only authed users can view

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Heading from './Heading';
+import { AUTH_TOKEN } from '../constants';
+import { Redirect } from 'react-router';
 
 const Note = () => {
   const title = "Note"
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  if (!authToken) {
+    alert("Please login to view note.")
+    return <Redirect to="/login" />
+  }
 
   return (
     <Heading title={title} />
