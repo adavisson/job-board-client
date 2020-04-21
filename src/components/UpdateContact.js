d75f8c0618735d7968158334a1b9e1233993985f
Andrew Davisson <akdavisson4@gmail.com>
2020-04-21 10:32:42 -0600

    stubbed out card and form

--- src/components/UpdateContact.js
+++ src/components/UpdateContact.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import { Button } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Query, Mutation } from 'react-apollo'
+import { Redirect } from 'react-router'
+import { Button, Card, Form } from 'react-bootstrap'
+import { AUTH_TOKEN } from '../constants'
 
 const UpdateContact = (props) => {
   const contactId = props.match.params.id
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [jobTitle, setJobTitle] = useState('')
+  const [companyId, setCompanyId] = useState('')
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
+  if (!authToken) {
+    alert('Please login to edit contact')
+    return <Redirect to="/login" />
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const updateForm = () => {
+    return (
+      <Form onSubmit={handleSubmit}>
+
+      </Form>
+    )
+  }
   
   return (
-    <Button variant="secondary" onClick={() => props.history.push(`/contacts/${contactId}`)}>Cancel</Button>
+    <div className="container">
+      <Card className="text-center">
+        <Card.Header>{contactId}</Card.Header>
+        <Card.Body>{updateForm()}</Card.Body>
+        <Card.Footer>
+          <Button variant="secondary" onClick={() => props.history.push(`/contacts/${contactId}`)}>Cancel</Button>
+        </Card.Footer>
+      </Card>
+    </div>
   );
 }
  
